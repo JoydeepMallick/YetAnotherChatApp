3d2ce07b1ec4f824360a1b5f989cf86c376634e9
Joydeep Mallick <joydeep150703@gmail.com>
2025-10-14 12:27:05 +0530

    update

--- src/routes/HomePage/HomePage.jsx
+++ src/routes/HomePage/HomePage.jsx
@@ -4,7 +4,31 @@ import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
     <div className="homepage">
-      <Link to="/dashboard">Dashboard</Link>
+      {/* background wallpaper */}
+      <img src="../public/orbital.png" alt="orbital" className="orbital" />
+
+      <div className="left">
+        <h1>Alpaca AI</h1>
+        <h2>Yet another ChatGPT clone but worth trying</h2>
+        <h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit,
+          mollitia soluta? Voluptates reprehenderit alias praesentium blanditiis
+          atque, laborum quasi, saepe est earum iusto necessitatibus!
+        </h3>
+        <Link to="/dashboard">Get Started</Link>
+      </div>
+
+      <div className="right">
+        <div className="imgContainer">
+          <div className="bgContainer">
+            {/* animated background */}
+            <div className="bg"></div>
+          </div>
+
+          {/* alpaca waifu image */}
+          <img src="../public/waifu.png" alt="alpacawaifu" className="waifu" />
+        </div>
+      </div>
     </div>
   );
 };
